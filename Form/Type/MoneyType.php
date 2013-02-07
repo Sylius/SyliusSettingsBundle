cb3d638f843a6009cab04c4149a1fb4ecffcfa65
Myke Hines <myke@webhines.com>
2013-02-07 11:51:53 -0800

    Support our currency value in forms so that they can be displayed according to locale configured in project

---
+++ Form/Type/MoneyType.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\SettingsBundle\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\MoneyType as BaseMoneyType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MoneyType extends BaseMoneyType
+{
+	/**
+	* {@inheritdoc}
+	*/
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+   	{
+        $resolver->setDefaults(array(
+            'precision' => 2,
+            'grouping'  => false,
+            'divisor'   => 1,
+            'currency'  => 'USD',
+            'compound'  => false,
+        ));
+    }
+
+
+
+}
