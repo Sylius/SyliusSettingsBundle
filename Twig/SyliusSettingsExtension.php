096da4905ee11ae96ccbe69d049e4b5a76a2b3cd
Myke Hines <myke@webhines.com>
2013-02-07 11:52:04 -0800

    Introduce twig filter "sylius_currency_format" so that we can localize number formating and currency symbol based on the projects configured settings Example:   {{ '6539.49' | sylius_currency_format }} Displays:   $6,539.49 OR   6.539,49 €

--- Twig/SyliusSettingsExtension.php
+++ Twig/SyliusSettingsExtension.php
@@ -14,6 +14,7 @@ namespace Sylius\Bundle\SettingsBundle\Twig;
 use Sylius\Bundle\SettingsBundle\Manager\SettingsManagerInterface;
 use Twig_Extension;
 use Twig_Function_Method;
+use Twig_Filter_Method;
 
 /**
  * Sylius settings extension for Twig.
@@ -29,6 +30,10 @@ class SyliusSettingsExtension extends Twig_Extension
      */
     private $settingsManager;
 
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
     /**
      * Constructor.
      *
@@ -48,6 +53,44 @@ class SyliusSettingsExtension extends Twig_Extension
             'sylius_settings_all' => new Twig_Function_Method($this, 'getSettings'),
             'sylius_settings_get' => new Twig_Function_Method($this, 'getParameter'),
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return array(
+            'sylius_currency_format' => new Twig_Filter_Method($this, 'formatCurrency')
+        );
+    }
+
+    /**
+     * @var ContainerInterface
+     */
+    public function setContainer($container)
+    {
+        $this->container = $container;
+
+    }   
+
+    /**
+     * @var number Currency amount to format
+     * @var string Optional currency to format to
+     * @var string Optional local value
+     *
+     * @return string Formatted currency value with currency type
+     */
+    public function formatCurrency($value, $currency = null, $locale = null)
+    {
+        if (!is_numeric($value))
+            return null;
+
+        $useLocale = $locale === null ? $this->container->get('request')->getLocale() : $locale;
+        $useCurrency = $currency === null ? $this->container->getParameter('currency') : $currency;
+
+        $formatter = new \NumberFormatter($useLocale, \NumberFormatter::CURRENCY);
+        return  $formatter->formatCurrency($value, $useCurrency);
     }
 
     /**
